6f557c78285a1365f22807b934333cb2b468c67a
Justice Nwokolo <nwokolougochukwu2000@gmail.com>
2024-11-05 14:50:59 +0100

    first commit

---
+++ school.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+   navLinks.style.right = "0";
+}
+function hideMenu() {
+   navLinks.style.right = "-200px";
+}
